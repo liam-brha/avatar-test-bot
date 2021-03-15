db7151dc146240f681301b78e9c9b87600ad6b56
textEditUser <runningzoomcat@gmail.com>
2021-03-15 18:52:52 +1100

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const axios = require("axios")
+
+client.on("ready", () => {
+	console.log(`success. username is ${client.user.tag}`)
+});
+
+client.on("message", msg => {
+	// there has to be a better way to do this but uh
+	if(msg.content.split(" ")[0] === ">avatar") {
+		if(msg.content.split(" ").length > 1) {
+			try {
+				client.users.fetch(msg.content.split("!")[1].split(">"[0])[0])
+				.then(result => msg.channel.send("https://cdn.discordapp.com/avatars/" + result.id +"/" + result.avatar + "?size=1024"))
+			}
+			catch(err) {
+				msg.channel.send("https://cdn.discordapp.com/avatars/" + msg.author.id +"/" + msg.author.avatar + "?size=1024")
+			}
+		}
+		else {
+			msg.channel.send("https://cdn.discordapp.com/avatars/" + msg.author.id +"/" + msg.author.avatar + "?size=1024")
+		}
+	}
+	if(msg.content.split(" ")[0] === ">count") {
+		let value = async () => {return await axios.get(process.env.myspcalapi)} // if you wondering what that api is go look at my other repo
+		value().then(result => msg.channel.send(result.data))
+	}
+})
+
+client.login(process.env.token)
