a9cc8eb262ef09d4691a1c7213cea3d0834a7a96
XcodeBoi <runningzoomcat@gmail.com>
2021-12-15 08:30:58 +1100

    testing

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const axios = require("axios")
+
+class cdnURL {
+	constructor(usrId, fileName) {
+		this.usrId = usrId;
+		this.fileName = fileName;
+	}
+	get fullURL() {
+		avatarURL = "https://cdn.discordapp.com/avatars/" + this.usrId +"/" + this.fileName + "?size=1024";
+	}
+}
+
+function isValidId(id) {
+	// Msg str at this point should be ">avatar <@!42838xxxxxxxxxxxxx>"
+	try {
+		// per message evalutation of if ok \\\ todo: add guild arg
+		client.guilds.get()
+		msg.content.split("!")[1].split(">"[0])[0]
+	}
+	catch(e) { return false; }
+}
+
+async function getCdnObj(messageAuthor, ARGS) {
+	if(ARGS.length && isValidId(ARGS[0])) {
+		return client.users.fetch(USRID)
+		.then(result => cdnURL(result.id, result.avatar));
+	}
+	else {
+		return cdnURL(messageAuthor.id, messageAuthor.avatar);
+	}
+}
 
 client.on("ready", () => {
-	console.log(`success. username is ${client.user.tag}`)
+	console.log(`success. username is ${client.user.tag}`);
 });
 
-client.on("message", msg => {
-	// there has to be a better way to do this but uh
-	if(msg.content.split(" ")[0] === ">avatar") {
-		if(msg.content.split(" ").length > 1) {
-			try {
-				client.users.fetch(msg.content.split("!")[1].split(">"[0])[0])
-				.then(result => msg.channel.send("https://cdn.discordapp.com/avatars/" + result.id +"/" + result.avatar + "?size=1024"))
-			}
-			catch(err) {
-				msg.channel.send("https://cdn.discordapp.com/avatars/" + msg.author.id +"/" + msg.author.avatar + "?size=1024")
-			}
-		}
-		else {
-			msg.channel.send("https://cdn.discordapp.com/avatars/" + msg.author.id +"/" + msg.author.avatar + "?size=1024")
-		}
-	}
-	if(msg.content.split(" ")[0] === ">count") {
-		axios.get(process.env.myspcalapi, {headers: {"usertype": "bot"}}).then(result => msg.channel.send(result.headers.number)) // if you wondering what that api is go look at my other repo
-	}
-	if(msg.content.split(" ")[0] === ">log") {
-		var array = []
-		msg.channel.messages.fetch({limit: 100}).then(result => {
-			array = result.map(currentValue => currentValue.author.username + ": " + currentValue.content)
-			for(i = array.length; i >= 0; i--) {
-				console.log(array[i])
-			}
-		})
-		
-		// increasing arrow function fluency
-		// and learning how to deal with maps
+client.on("message", async msg => {
+	const ARGS = msg.content.split(" ").pop(0);
+	const COMMAND = msg.content.split(" ")[0];
+	
+	if(COMMAND == ">avatar") {
+		let cdnObj = await getCdnObj(msg.author, ARGS);
+		msg.channel.send(cdnObj.fullURL());
 	}
 })
 
-client.login(process.env.token) // heroku enviroment varibles
+client.login(process.env.token)
